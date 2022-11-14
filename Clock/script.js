f09903ce7ea35e1f64e0ed10e6ba968e6c50e929
MohammadMzien2 <mohammadlu281@gmail.com>
2022-11-14 08:20:41 +0100

    Clock checeker

--- Clock/script.js
+++ Clock/script.js
@@ -8,10 +8,12 @@ const tick = () => {
 	// output current time to `#clock`-element
 	clockEl.innerText = now.toLocaleTimeString();
 
-	if (now.getDay() === 5 && now.getHours() >= 15) {
+	if (now.getDay() ===  7 && now.getHours() >= 15) {
 		clockEl.innerText += ' 🥳';
 	}
+
 }
+
 
 // we could stop the time by calling `clearInterval(clockId)`
 // start interval-timer with 1000 ms interval
