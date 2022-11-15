2778490c6f6cea09d5995c5ff83e4a6d7b47682b
MohammadMzien2 <mohammadlu281@gmail.com>
2022-11-15 14:12:53 +0100

    Filter all numbers and name

--- Higher Order Array Methods/filter.js
+++ Higher Order Array Methods/filter.js
@@ -21,14 +21,53 @@ numbers.forEach(num =>{
         largeNumbers.push(num);
     }
 });
-*/
+
 
 // filter out all numbers större än => 25
-const largeNumbers = numbers.filter((num) => {  
+const largeNumbers = numbers.filter((num) => { 
+    return num >= 25;
+
     if (num >= 25) {
         return true; //Yes this number should be in the new array 
     } else {
         return false; // NOOO, exclude this number from the new array
     }
 });
+
+// förkortning
+const largeNumbers = numbers.filter(num>= num>= 25);
 console.log("lagre numbers:", largeNumbers);
+*/
+
+/*
+const names = ["Johan", "kajsa", "Bo", "Li"];
+const longishNames = names.filter(name => name.length > 2);
+console.log("Longish names:", longishNames);
+*/
+
+const students = [
+    {
+        name: "johan",
+        points: 1337,
+    },
+    {
+        name: "Saman",
+        points: 3,
+    },
+    {
+        name: "alicia",
+        points: 42,
+    },
+    {
+        name: "Elliot",
+        points: 88,
+    },
+    {
+        name: "Maja",
+        points: 35,
+    },
+];
+
+// vilka studenter som är godkända
+const passed = students.filter(student => student.points >= 20);
+console.log("Students who passed the exam:", passed)
