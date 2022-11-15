90a8ecb114556a1cb014d7598e6adbe1d8d7b3fb
MohammadMzien2 <mohammadlu281@gmail.com>
2022-11-15 14:33:08 +0100

    find first large number

---
+++ Higher Order Array Methods/find.js
@@ -0,0 +1,40 @@
+/*
+.sort()
+.fillter()
+.find()
+.map()
+.reduce()
+*/
+//  clone an Array
+//  Shuffle an Array
+//numbers 
+
+const numbers = [49, 5, 21, 50, 24, 20, 47, ]
+
+// find fist large number
+const largeNumbers = numbers.filter(num => num=> 25);
+const firstLargeNumber = numbers.find(num => num=> 25); // 50
+
+
+const students = [
+    {
+        name: "johan",
+        points: 1337,
+    },
+    {
+        name: "Saman",
+        points: 3,
+    },
+    {
+        name: "alicia",
+        points: 42,
+    },
+    {
+        name: "Elliot",
+        points: 88,
+    },
+    {
+        name: "Maja",
+        points: 35,
+    },
+];
