797b58d1ebe438c19f33007ce7942d7357ac66df
MohammadMzien2 <mohammadlu281@gmail.com>
2022-11-17 19:18:55 +0100

    Shaun Map

--- Higher Order Array Methods/map.js
+++ Higher Order Array Methods/map.js
@@ -19,7 +19,7 @@ console.log("Numbers:", numbers);
 // multiply all the numbers by 2
 const doubleNumbers = numbers.map(num => num * 2);
 console.log("Double? numbers:", doubleNumbers);  // [ 10, 42, 100, 48, 184, 36, 94 ]
-*/
+
 
 const products = [
 	{
@@ -54,7 +54,7 @@ products.forEach(product => {
 	skus.push(product.sku);
 });
 console.log("All SKUs:", skus);
-*/
+
 
 const skus = products.map( product => {
 	return product.sku;
@@ -63,3 +63,25 @@ console.log("All SKUs, but with map:", skus);
 
 const productNames = products.map(product => product.name);
 console.log("All product names:", productNames);
+*/
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+
+// const salePrices = prices.map((price)=>  price / 2);
+// console.log(salePrices);
+
+const products = [
+    {name: 'gold star',  price: 20},
+    {name: ' mushroom', price: 40 },
+    {name: 'green shell', price: 30},
+    {name: 'banana skin', price: 10 },
+	{name: 'red shells', price: 50 }
+];
+
+const saleProducts = products.map ((product)=>{
+	if(product.price > 30){
+		return {name: product.name, price: product.price / 2};
+	}else {
+		return product;
+	}
+});
+console.log(saleProducts, products);
