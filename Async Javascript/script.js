e5ff3f4d70cf89d76acb589e4d89114b3aa7e322
MohammadMzien2 <mohammadlu281@gmail.com>
2022-11-18 22:34:31 +0100

    Promises

--- Async Javascript/script.js
+++ Async Javascript/script.js
@@ -13,33 +13,66 @@ console.log(4);
 
 // Make Http request 
 
-const getTodos = () =>{
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+
+    return new Promise((resolve, reject) => {
+
+        const request = new XMLHttpRequest();
 
 
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200){
-        callback(undefined, request.responseText);
-    }else if (request.readyState === 4){
-        callback('could not fetch data', undefined);
-    }
-});
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource')
+            }
+        });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+        request.open('GET', resource);
+        request.send();
+    });
 };
-
-console.log(1);
-console.log(2);
-
-getTodos((err, data) => {
-console.log('callback fired');
-if(err){
-    console.log(err);
-}else {
-    console.log(data);
-}
+getTodos('luigi.json').then(data => {
+    console.log('promise resolved:', data);
+    return getTodos('mario.json');
+}).then( data => {
+console.log('proise 2 resolved:', data)
+}).catch(err => {
+    console.log('promise rejected:', err)
 });
 
-console.log(3);
-console.log(4);
+
+
+
+// Promise example 
+/*
+const getSomething = () => {
+
+    return new Promise((resolve, reject) => {
+        // Fetch something 
+        resolve('some data');
+
+
+        //reject('some error');
+    });
+};
+// så här gör man resolve 
+ getSomething().then((data) => {
+     console.log(data);
+ });
+ 
+// så här gör man reject
+  getSomething().then((data) => {
+          console.log(data);
+  }, (err) => {
+     console.log(err);
+  });
+
+// getSomething().then(data => {
+ console.log(data);
+ }).catch(err => {
+     console.log(err);
+ });
+
+*/
