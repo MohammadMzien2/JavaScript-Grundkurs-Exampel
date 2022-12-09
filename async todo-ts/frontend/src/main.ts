8ee25fe89ab2b0e17f62ba390e64f7d6cc41adba
MohammadMzien2 <mohammadlu281@gmail.com>
2022-12-09 14:40:28 +0100

    Show unfinshed stuff

--- async todo-ts/frontend/src/main.ts
+++ async todo-ts/frontend/src/main.ts
@@ -1,42 +1,61 @@
-import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import './style.css'
 
 interface ITodo {
-    id: number,
-    title: string,
-    completed: boolean,
+	id: number,
+	title: string,
+	completed: boolean,
 }
-// Local variable containing all the todos from the server 
+
+// Local variable containing all the todos from the server
 let todos: ITodo[] = []
 
-// Fetch todos from server
+/**
+ * Fetch todos from server
+ */
 const fetchTodos = async () => {
-    const res = await fetch('http://localhost:3001/todos')
-    if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`)
-    }
-    return await res.json() as ITodo[]
+	const res = await fetch('http://localhost:3001/todos')
+	if (!res.ok) {
+		throw new Error(`${res.status} ${res.statusText}`)
+	}
 
-
+	return await res.json() as ITodo[]
 }
 
-// Get todos from server, update `todos`-array and render todos.
+/**
+ * Get todos from server, update `todos`-array and render todos.
+ */
 const getTodos = async () => {
-    // Fetch todos from server and update local copy    
-    todos = await fetchTodos()
+	// Fetch todos from server and update local copy
+	todos = await fetchTodos()
 
-    // Update `todos`-array
-    renderTodos()
-
-    // Render todos
-
+	// Render todos
+	renderTodos()
 }
 
-// Render todos to DOM
+/**
+ * Render todos to DOM
+ */
 const renderTodos = () => {
-    document.querySelector("#todos")!.innerHTML =
-    todos.map(todo => `<li>${todo.title}</li> `)
-    .join('')
+	// Render not completed todos
+	document.querySelector('#todos')!.innerHTML = todos
+		.filter(todo => !todo.completed)
+		.map(todo => `
+			<li class="list-group-item">
+				${todo.title}
+			</li>
+		`)
+		.join('')
+
+	// Render completed todos
+	document.querySelector('#completed-todos')!.innerHTML = todos
+		.filter(todo => todo.completed)
+		.map(todo => `
+			<li class="list-group-item">
+				${todo.title}
+			</li>
+		`)
+		.join('')
 }
 
 getTodos()
