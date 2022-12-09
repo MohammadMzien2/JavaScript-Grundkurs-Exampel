317a86347aae894a0b5d173a8f5fd78933550f16
MohammadMzien2 <mohammadlu281@gmail.com>
2022-12-09 11:38:15 +0100

    Fetch todo from server

--- async todo-ts/frontend/src/main.ts
+++ async todo-ts/frontend/src/main.ts
@@ -1,3 +1,32 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+interface ITodo {
+    id: number,
+    title: string,
+    completed: boolean,
+}
+
+// Fetch todos from server
+const fetchTodos = async () => {
+    const res = await fetch('http://localhost:3001/todos')
+    if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`)
+    }
+    return await res.json() as ITodo[]
+
+
+}
+
+// Get todos from server, update `todos`-array and render todos.
+const getTodos = async () => {
+    // Fetch todos from server
+    const omgdata = await fetchTodos()
+    console.log(omgdata)
+
+    // Update `todos`-array
+
+    // Render todos
+}
+
+getTodos()
