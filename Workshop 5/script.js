6fbdcb61aee4f562dab0e82d97e69cbe611748d0
MohammadMzien2 <mohammadlu281@gmail.com>
2022-12-09 10:45:34 +0100

    Johan todos

--- Workshop 5/script.js
+++ Workshop 5/script.js
@@ -18,27 +18,55 @@
  */
 
 // get references to DOM elements
+// get references to DOM elements
 const todosEl = document.querySelector('#todos');
 const newTodoFormEl = document.querySelector('#new-todo-form');
 
 // list of todos
-const todos = [
-	{
-		id: 1,
-		title: "Learn basic JavaScript",
-		completed: true,
-	},
-	{
-		id: 2,
-		title: "Learn Array Methods",
-		completed: false,
-	},
-	{
-		id: 3,
-		title: "Take over the world",
-		completed: false,
-	},
-];
+let todos = [];
+
+const createNewTodo = async (newTodo) => {
+	const res = await fetch('http://localhost:3001/todos', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(newTodo),
+	});
+
+	// Check that everything went ok
+	if (!res.ok) {
+		throw new Error(`Could not create todo, reason: ${res.status} ${res.statusText}`);
+	}
+
+	return await res.json();
+}
+
+/**
+ * Get todos from server, update `todos` array and render todos.
+ */
+const getTodos = async () => {
+	// Fetch todos from server
+	const data = await fetchTodos();
+
+	// Set `todos` array to the data we got from the server
+	todos = data;
+
+	// Render the todos
+	renderTodos();
+}
+
+/**
+ * Fetches todos from server.
+ */
+const fetchTodos = async () => {
+	const res = await fetch('http://localhost:3001/todos');
+	if (!res.ok) {
+		throw new Error(`Could not fetch todos, reason: ${res.status} ${res.statusText}`);
+	}
+
+	return await res.json();
+}
 
 // Render todos to DOM
 const renderTodos = () => {
@@ -60,20 +88,33 @@ const renderTodos = () => {
 
 	todosEl.innerHTML = lis.join('');
 }
-renderTodos();
+
+/**
+ * Update an existing todo on the server.
+ */
+const updateTodo = async (todoId, data) => {
+	const res = await fetch(`http://localhost:3001/todos/${todoId}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+
+	// Check that everything went ok
+	if (!res.ok) {
+		throw new Error(`Could not update todo, reason: ${res.status} ${res.statusText}`);
+	}
+
+	return await res.json();
+}
 
 // Listen for click-events on `#todos` (the `<ul>`)
-todosEl.addEventListener('click', (e) => {
-	// console.log("You clicked on either the whole list, or on a listitem", e.target);
-
+todosEl.addEventListener('click', async (e) => {
 	// check if user clicked on a LI element
 	if (e.target.tagName === "LI") {
-		// console.log("YAY you clicked on a todo (LI)", e.target);
-		// console.log("The clicked todo's title is:", e.target.innerText);
-
 		// get the `data-todo-id` attribute from the LI element
 		const clickedTodoId = e.target.dataset.todoId;     // `data-todo-id`
-		// console.log("You clicked on the listitem for todo with id:", clickedTodoId);
 
 		// search todos for the todo with the id todoId
 		const clickedTodo = todos.find( (todo) => {
@@ -82,72 +123,49 @@ todosEl.addEventListener('click', (e) => {
 		console.log("found clicked todo", clickedTodo);
 
 		// change completed-status of found todo
-		clickedTodo.completed = !clickedTodo.completed;
+		await updateTodo(clickedTodo.id, {
+			completed: !clickedTodo.completed
+		});
+
 		console.log("toggling todo completed");
 
-		// render updated todos
-		renderTodos();
+		// get updated todos
+		getTodos();
 	}
 });
 
 // Create a new todo when form is submitted
-newTodoFormEl.addEventListener('submit', (e) => {
+newTodoFormEl.addEventListener('submit', async (e) => {
 	// Prevent form from being submitted (to the server)
 	e.preventDefault();
 
-	// Extract all todo ids
-	// const todoIds = todos.map(todo => todo.id);    // [1, 2, 3]
-	// const maxTodoId = Math.max(...todoIds);   // 3
-	// const newTodoId = maxTodoId + 1;    // 4
-
-	const maxTodoId = todos.reduce((maxId, todo) => {
-		return Math.max(todo.id, maxId);
-
-		// return (todo.id > maxId)
-		// 	? todo.id
-		// 	: maxId;
-
-		// if (todo.id > maxId) {
-		// 	return todo.id;
-		// }
-
-		// return maxId;
-	}, 0);
-	const newTodoId = maxTodoId + 1;    // 4
-
-	// Create and push new todo into array
-	todos.push({
-		id: newTodoId,
+	// Create new todo
+	const newTodo = {
 		title: newTodoFormEl.newTodo.value,
 		completed: false,
-	});
-	console.log("created new todo...");
+	}
 
-	// Render new todo to DOM
-	renderTodos();
+	// POST todo to server
+	try {
+		await createNewTodo(newTodo);
+	} catch (e) {
+		console.log(e);
+		alert(e);
+	}
 
-	// Empty input field
-	// newTodoFormEl.newTodo.value = '';
+	// Get the new list of todos from the server
+	getTodos();
 
 	// Reset form
 	newTodoFormEl.reset();
 });
 
-/*
-// STOP USER FROM RESETTING FORM 😈
-newTodoFormEl.addEventListener('reset', e => {
-	// YOU NO RESET FORM, FORM RESETS YOU!
-	e.preventDefault();
+// Get and render todos
+getTodos();
 
-	alert("YOU NO RESET FORM, FORM RESETS YOU!");
-});
-*/
+
 
 /*
-
-
-
-
 // Annan sätt och göra den på
 
 const addForm = document.querySelector('add');
